9a9f3dbb6465da57dabe02b7f8c1d9544daea517
Daon Kang <gracekang253@koreacu.ac.kr>
2025-10-18 23:04:09 +0900

    Create 120803.py

---
+++ programmers/120803.py
@@ -0,0 +1,15 @@
+# 두 수의 차 구하기
+
+# 120803
+
+# 문제
+# 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 return하도록 soltuion 함수를 완성해주세요.
+
+# 입출력 예
+# num1	num2	result
+# 2  	3	-1
+# 100	2	98
+
+def solution(num1, num2):
+    answer = num1 - num2
+    return answer
