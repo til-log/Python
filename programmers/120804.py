6dca30f3ca006ab533175cc98915e106ea5fcc19
Daon Kang <gracekang253@koreacu.ac.kr>
2025-10-18 23:07:05 +0900

    Create 120804.py

---
+++ programmers/120804.py
@@ -0,0 +1,16 @@
+# 두 수의 곱 구하기
+
+# 120804
+
+# 문제
+# 정수 num1, num2가 매개변수 주어집니다. num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+
+# 입출력 예
+# num1	num2	result
+# 3	     4	12
+# 27	19	513
+
+
+def solution(num1, num2):
+    answer = num1 * num2
+    return answer
