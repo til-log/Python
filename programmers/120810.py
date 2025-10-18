9846b4ed548216ec29054734f413273e626e2d53
Daon Kang <gracekang253@koreacu.ac.kr>
2025-10-18 23:02:35 +0900

    Create 120810.py

---
+++ programmers/120810.py
@@ -0,0 +1,23 @@
+# 나머지 구하기
+
+# 120810
+
+# 문제 설명
+# 정수 num1, num2가 매개변수로 주어질 때, num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
+
+# 입출력 예
+# num1	num2	result
+# 3	2	1
+# 10	5	0
+
+# 파이썬
+# / 실수 나눗셈
+# // 정수 나눗셈 => 소수점 이하 버린 몫
+# % 나머지 연산
+
+
+def solution(num1, num2):
+    answer = num1 % num2
+    return answer
+
+
