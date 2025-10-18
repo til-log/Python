720ae9100bee93f5d8bd25257563144a6e963ffd
Daon Kang <gracekang253@koreacu.ac.kr>
2025-10-18 23:13:57 +0900

    Update 120802.py
    다른 사람 결과 확인하여 공부 진행
    파이썬 튜토리얼 다시 봐두면 좋을 것 같음

--- programmers/120802.py
+++ programmers/120802.py
@@ -13,3 +13,12 @@
 def solution(num1, num2):
     answer = num1 + num2
     return answer
+
+
+# ------
+# 가변인자와 람다 방식으로도 가능
+def solution(*x):
+    return sum(x)
+# *args 활용으로 가변 인자를 잡아 num1, num2를 전부 포함(인자 개수 안정함을 제시)
+# solution = lambda *x : sum(x)
+# 으로 인자 여러 개 받는 것을 나타낼 수 있음
