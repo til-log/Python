cda7ffc0e814d3369c79673383782e36a74f6e7e
Daon Kang <gracekang253@koreacu.ac.kr>
2025-10-18 23:05:32 +0900

    Create 120802.py

---
+++ programmers/120802.py
@@ -0,0 +1,15 @@
+# 두 수의 합 구하기
+
+# 120802
+
+# 문제
+# 정수 num1과 num2가 주어질 때, num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+
+# 입출력 예
+# num1	num2	result
+# 2	 3	5
+# 100	2	102
+
+def solution(num1, num2):
+    answer = num1 + num2
+    return answer
